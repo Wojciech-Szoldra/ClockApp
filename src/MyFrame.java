1e22c41f92d59f6adee68aec593c2909f2184674
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2023-06-19 10:57:11 +0200

    First local commit

---
+++ src/MyFrame.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MyFrame extends JFrame {
+
+    Calendar calendar;
+    SimpleDateFormat timeFormat;
+    JLabel timeLabel;
+    String time;
+
+    MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Clock App");
+        this.setLayout(new FlowLayout());
+        this.setSize(350,200);
+        this.setResizable(false);
+
+        timeFormat = new SimpleDateFormat("hh:mm:ss a");
+        timeLabel = new JLabel();
+
+        this.add(timeLabel);
+        this.setVisible(true);
+
+        setTime();
+    }
+
+    public void setTime() {
+        while(true) {
+            time = timeFormat.format(Calendar.getInstance().getTime());
+            timeLabel.setText(time);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
